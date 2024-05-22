b19eba433c37ba2b1ce9916a4602729e0762a012
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-05-22 10:44:24 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='h-40 text-3xl text-purple-800 font-bold text-center'>This is a heading</h1>
+    </>
+  )
+}
+
+export default App
