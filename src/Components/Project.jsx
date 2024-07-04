37ad8acea01c38e70d1aea206008133f2f29543c
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-07-04 14:04:06 +0530

    ADD new project

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Project1 from './Projects/Project1'
 import Project2 from './Projects/Project2'
 import Project3 from './Projects/Project3'
+import Project4 from './Projects/Project4'
 
 function Project() {
   return (
@@ -9,6 +10,7 @@ function Project() {
         <p className='mx-auto text-sm font-newFont text-gray-600 md:text-base dark:text-gray-300'>Browse My Recent</p>
         <p className='mx-auto text-3xl font-bold font-newFont md:text-4xl xl:text-5xl dark:text-white'>Projects</p>
         <div className='w-[90%] mx-auto h-auto flex flex-wrap gap-5 my-5 justify-center'>
+            <Project4 />
             <Project1 />
             <Project2 />
             <Project3 />
