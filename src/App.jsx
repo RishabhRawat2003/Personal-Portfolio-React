8428719bf098e702e42514fc41e527a53aa203b0
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-07-24 16:52:23 +0530

    adds web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './Components/About'
 import Project from './Components/Project'
 import Contact from './Components/Contact'
 import Footer from './Components/Footer'
-
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -14,6 +14,7 @@ function App() {
       <div className='h-auto w-full bg-slate-100 dark:bg-slate-700'>
         <Header />
         <MainBody />
+        <Analytics />
         <About />
         <Project />
         <Contact />
